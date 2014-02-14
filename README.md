7497cba9fea42ee0ecbd7c335a09d1178ee9e137
Cazum <jeremyclark@live.ca>
2014-02-14 18:39:00 -0500

    Added pattern matching, regex improvement, optimizations, etc
    Added pattern matching (trailing nums, common words)

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ spass.js
 
 Realtime javascript-based password strength checker
 
-![screenshot](http://i.imgur.com/Zd8HG74.gif)
+![screenshot](http://i.imgur.com/7diop2m.gif)
 
 [Try out the demo](http://codezombie.github.io/spass.js)
 
 ###about
 spass.js is a small function written for websites to help their soon-to-be users create a strong and secure password.
-spass.js checks for a long password length (~12), a mix of uppercase letters, lowercase letters, numbers, and special characters. In order to receive a good score, a password must meet all of these requirements, or make up for lacking requirements with exceptionally length (for example, "Th3HoUs3F!rE" would score the same as "TheHouseFireTheHouseFire"). spass.js also checks for certain patterns that are commonly looked for by brute-forcing software. For example, numbers on the end won't increase the score as much as if they were in the center of the password. Note: spass.js will not check for dictionary words. This wouldn't be practical for a javacript file. I suggest an ajax->server-side-script to avoid having to send a dictionary file to the user.
+spass.js checks for a long password length (~12), a mix of uppercase letters, lowercase letters, numbers, and special characters. In order to receive a good score, a password must meet all of these requirements, or make up for lacking requirements with exceptionally length (for example, "Th3HoUs3F!rE" would score the same as "thehousefirehello"). spass.js also checks for certain patterns that are commonly looked for by brute-forcing software. For example, numbers on the end won't increase the score as much as if they were in the center of the password, and common any matches to a common phrase or password will reduce the score drastically. Note: spass.js will not check for full dictionary matches. This wouldn't be practical for a javacript file. I suggest an ajax->server-side-script to avoid having to send an entire dictionary file to the user.
 
 ###usage
 + include spass.js in your html page with `<script type="text/javascript" src="../spass.min.js"></script>`
--- source/spass.js
+++ source/spass.js
@@ -1,97 +1,191 @@
+/*
+spass.js (securepass) by Cazum (zombiearmy.net)
+
+	spass.js works by calculating the total bits of entropy for your given 
+password(log2(Charset) * L), and comparing it against what many would regard as
+a "safe" password (log2(94) bits of entropy  multiplied by 12 character length).
+
+	spass.js also checks for common passwords, and deducts score from that (only 
+awarding score to the variations applied on top of said common password), and for 
+common patterns that a brute-force program might look for (numbers at the end of 
+a password, etc) and reduces score accordingly.
+
+* MIT LICENSE
+* Copyright (C) 2014 by Jeremy Clark
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
 function checkpassword(pass) {
-	var commonpasswords = new Array("123456","1234567","12345678","123456789","1234567890","password","qwerty","abc123","111111","iloveyou","adobe123","admin","letmein","monkey","shadow","sunshine","password1","princess","azerty","trustno1","000000","liverpool","charlie","696969","master","superman","harley","mustang","football","baseball","hunter2");
-	
+	var commonpasswords = new Array("123",
+									"456",
+									"789",
+									"secret",
+									"internet",
+									"password",
+									"dragon",
+									"qwerty",
+									"abc123",
+									"iloveyou",
+									"letmein",
+									"adobe",
+									"cookie",
+									"coffee",
+									"biteme",
+									"admin",
+									"monkey",
+									"shadow",
+									"sunshine",
+									"computer",
+									"princess",
+									"azerty",
+									"trustno1",
+									"liverpool",
+									"charlie",
+									"6969",
+									"master",
+									"superman",
+									"harley",
+									"mustang",
+									"football",
+									"baseball",
+									"hunter",
+									"beaver",
+									"banana",
+									"chicken",
+									"whatever",
+									"welcome",
+									"superman",
+									"hotdog",
+									"iloveyou",
+									"cowboy");
 	var strength = 0;
 	var score = 0;
 	var charset = 0;
 	var password = pass;
 	var password_length = password.length;
 	
-	var trailing_numbers = 0;
-	
 	var message = "";
 	
-	//check if password is in the common list
-	for (var w in commonpasswords) {
-		if(password.toLowerCase() == commonpasswords[w]) {
-			message = "Password too common.";
-			score = 0;
-			return Array(score, message);
+	var trailing_numbers = 0;
+	var common_password_count = 0;
+	var common_password_search_loop = true; //initiate the recursive search loop
+
+	//check if password contains a common phrase
+	while(common_password_search_loop == true) {
+		var found = false; 
+		for(var x in commonpasswords) {
+			if(password.indexOf(commonpasswords[x]) !== -1) {
+				common_password_count += 1;
+				password_length -= commonpasswords[x].length;
+				password = password.replace(commonpasswords[x],"");
+				found = true; //found a common password, so keep searching for more.
+			}
+		}
+		if(found == false) {
+			common_password_search_loop = false; //couldn't find any common passwords, so break the loop.
+		}
+	}
+	if(common_password_count > 0) {
+		strength = (Math.log(50)/Math.log(2)) * common_password_count;// our list is derived from the top 50 passwords
+		if(common_password_count > 1) {
+			message += "common passwords found, ";
+		}
+		else {
+			message += "common password found, ";
 		}
 	}
 	
 	//check if password has trailing numbers and score them separately
-	if(/[0-9]+$/.test(password)) {
-		trailing_numbers = password.match(/[0-9]+$/)[0].toString().length;
+	if(/[^0-9]{1,}([0-9]{1,})$/g.test(pass)) {
+		trailing_numbers = /[^0-9]{1,}([0-9]{1,})$/g.exec(pass)[1].toString().length;
 		password_length -= trailing_numbers;
 		strength += (Math.log(10)/Math.log(2))*trailing_numbers;
 		password = password.slice(0,0-trailing_numbers);
-		if(message != "") { message += ", "; }
-		message += "numbers are less effective on the end";
+		message += "numbers are less effective on the end, ";
 	}
+	//TODO: 
+	
+		//check if password ends with a ? or !
+	
+		//check if first letter is a capital
+	
+		//check for three repeating characters (helllo, passsword, hi999)
 	
 	if(/[0-9]/.test(password)) {
 		charset += 10;
 	}
 	else {
-		if(message != "") { message += ", "; }
-		message += "no numbers in base of password";
+		message += "no numbers in base of password, ";
 	}
 	
 	if(/[a-z]/.test(password)) {
 		charset += 26;
 	}
 	else {
-		if(message != "") { message += ", "; }
-		message += "no lowercase letters";
+		message += "no lowercase letters, ";
 	}
 	
 	if(/[A-Z]/.test(password)) {
 		charset += 26;
 	}
 	else {
-		if(message != "") { message += ", "; }
-		message += "no uppercase letters";
+		message += "no uppercase letters, ";
 	}
 	
 	if(/[^a-zA-Z0-9]/.test(password)) {
 		charset += 32;
 	} 
 	else {
-		if(message != "") { message += ", "; }
-		message += "no special characters";
+		message += "no special characters, ";
 	}
 
 	//Length
 	if(password_length <= 7) {
-		if(message != "") { message += ", "; }
-		message += "too short";
+		message += "too short, ";
 	}
 	else if(password_length >= 8 && password_length < 10) {
-		if(message != "") { message += ", "; }
-		message += "length is passable";
+		message += "length is passable, ";
 	}
 	else if(password_length >= 10 && password_length < 12) {
-		if(message != "") { message += ", "; }
-		message += "could be longer";
+		message += "could be longer, ";
 	}
-
+	
+	if(charset > 0) {
+		strength += (Math.log(charset)/Math.log(2))*password_length;
+	}
+	score = strength / 78.65;//78.65 = log2(94) * 12
+	score = Math.round(score * 100);
+	
+	if(score > 100) {
+		score = 100;
+	}
+	
+	//Strip out any trailing ", "
+	if(message.charAt(message.length-2) == ",") {
+		message = message.slice(0,message.length-2);
+	}
+	
+	//Add a "." to the end of the message
 	if(message != "") {
 		message += ".";
 	}
 	
-	if(charset != 0) {
-		strength += (Math.log(charset)/Math.log(2))*password_length;
-		score = strength / ((Math.log(94)/Math.log(2))*12);
-		score = Math.round(score * 100);
-		if(score > 100) {
-			score = 100;
-		}
-	}
-	else {
-		score = 0;
-	}
-
+	//Capitalize the first word
 	message = message.charAt(0).toUpperCase() + message.substring(1);
 	
 	return Array(score,message);
